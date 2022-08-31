e84e3bcadfccb83db395664a676fda9bf0a7019a
ayoer <ayhnerylmz@gmail.com>
2022-08-31 18:36:41 +0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'antd/dist/antd.css';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='App_logo'>Welcome SST TEK TEST</div>
+    </>
+  );
+}
+
+export default App;
